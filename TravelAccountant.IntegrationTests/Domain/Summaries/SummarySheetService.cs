0c9a5f8e72db7c249ef196ace8cfa7aa38a05e3d
Ja-Rek <dto.jarek@gmail.com>
2019-06-02 09:42:19 +0200

    Add integration tests of 'ISummarySheetService'

--- TravelAccountant.IntegrationTests/Domain/Summaries/SummarySheetService.cs
+++ TravelAccountant.IntegrationTests/Domain/Summaries/SummarySheetService.cs
@@ -1,19 +1,55 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoFixture;
 using NUnit.Framework;
+using TravelAccountant.Domain.Moneys;
 using TravelAccountant.Domain.Summaries;
+using TravelAccountant.Domain.Summaries.Ryanair;
 
 namespace TravelAccountant.IntegrationTests.Domain.Summaries
 {
     internal class SummarySheetServiceTest
     {
-        [Test]
-        public void Test()
-        {
-            var summary = SummaryShetService();
+        const string DIRECTORY = "/home/jk/projects/AccountantAssistant/TravelAccountant."
+            + "IntegrationTests/Domain/Summaries/Files/";
 
-            Assert.Pass();
+        const string fileEuro = DIRECTORY+ "TestEuro.xlsx";
+        const string filePln = DIRECTORY+ "TestPln.xlsx";
+
+        [SetUp]
+        public void SetUp()
+        {
+            if (File.Exists(fileEuro)) File.Delete(fileEuro);
+            if (File.Exists(filePln)) File.Delete(filePln);
         }
 
-        public ISummarySheetService SummaryShetService()
+        [Test]
+        public void WriteSummariesToSheet_WhenSaveingSummariesTofile_ThenCreateCorrectFilesOnDisk()
+        {
+            SummaryShetService().WriteSummariesToSheet(Summaries(), DIRECTORY + "Test.xlsx");;
+
+            Assert.True(File.Exists(DIRECTORY+ "TestEuro.xlsx"));
+            Assert.True(File.Exists(DIRECTORY+ "TestPln.xlsx"));
+        }
+
+        private IEnumerable<BookingSummary> Summaries()
+        {
+            var summaries = SummariesByCurrency(Currency.EURO);
+            summaries.AddRange(SummariesByCurrency(Currency.PLN)); 
+
+            return summaries;
+        }
+
+        private List<BookingSummary> SummariesByCurrency(string currency)
+        {
+            var fixture = new Fixture();
+            fixture.Register(() => new Money(15, currency));
+
+            return fixture.CreateMany<BookingSummary>(5).ToList();
+        }
+
+        private ISummarySheetService SummaryShetService()
         {
             return ServiceLocator.Resolve<ISummarySheetService>();
         }
