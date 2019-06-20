5c6f8853587adc7c1acfeca1103fb374d9edbf57
Ja-Rek <dto.jarek@gmail.com>
2019-06-20 15:59:23 +0200

    Clean up, add logger

--- api/src/Bootstrap/Startup.cs
+++ api/src/Bootstrap/Startup.cs
@@ -13,14 +13,13 @@ namespace AspBootstrap
 {
     public class Startup
     {
-         public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -31,20 +30,15 @@ namespace AspBootstrap
             builder.RegisterAssemblyModules(Assembly.GetAssembly(typeof(ConfirmationsIOC)));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
-            }
 
-            //app.UseHttpsRedirection();
+
+            LogManager.Configuration = LogConfig.Config();
             app.UseMvc();
         }
     }
